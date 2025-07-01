525ab6e4551391cadaa261de3690b22fe5c7be70
Ivar Gavelin <ivargavelin@gmail.com>
2025-07-01 10:40:52 +0200

    cache rm

--- frontend/src/app/services/book.ts
+++
@@ -1,8 +0,0 @@
-export interface Book {
-  id: number;
-  title: string;
-  author: string;
-  publishedDate: string;
-  imageUrl: string;
-  userId: number;
-}
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://booktracker-n4h6.onrender.com/api'
-};
