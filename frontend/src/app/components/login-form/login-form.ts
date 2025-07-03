8148f3c8442488376bebed1b12f10d804b4b8802
Ivar Gavelin <ivargavelin@gmail.com>
2025-07-03 10:55:38 +0200

    new login loading

--- frontend/src/app/components/login-form/login-form.ts
+++ frontend/src/app/components/login-form/login-form.ts
@@ -34,7 +34,6 @@ export class LoginForm implements OnInit {
 
   async handleGoogleLogin(response: any): Promise<void> {
     const idToken = response.credential;
-    this.loading = true; 
 
     try {
       const res: any = await firstValueFrom(this.authService.googleLogin(idToken));
@@ -58,5 +57,9 @@ export class LoginForm implements OnInit {
     } finally {
       this.loading = false;
     }
+  }
+
+  startLoading() {
+    this.loading = true;
   }
 }
