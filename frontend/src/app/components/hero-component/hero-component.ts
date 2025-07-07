e1ec75af53affd49e8833cb5c79b5b4e3aba7bd7
Ivar Gavelin <ivargavelin@gmail.com>
2025-07-07 20:02:25 +0200

    profile hero add

--- frontend/src/app/components/hero-component/hero-component.ts
+++ frontend/src/app/components/hero-component/hero-component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Character } from '../../services/character';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-hero-component',
-  imports: [],
+  selector: 'app-hero',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero-component.html',
-  styleUrl: './hero-component.scss'
+  styleUrls: ['./hero-component.scss']
 })
 export class HeroComponent {
+  character$: Observable<Character | null>;
 
+  constructor(private authService: AuthService) {
+    this.character$ = this.authService.character$;
+  }
 }
